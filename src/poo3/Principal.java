e883d2acb332916e29b8f34c81def1fdc58d69a6
Samuel Loureiro <slcardoso.docente@gmail.com>
2022-12-20 23:02:31 +0100

    poo3 ejercicio 3

--- src/poo3/Principal.java
+++ src/poo3/Principal.java
@@ -1,6 +1,7 @@
 package poo3;
 
 import java.util.Scanner;
+import poo3.ejercicio3.Coche;
 import poo3.ejercicios1y2.Persona;
 
 /**
@@ -9,7 +10,10 @@ import poo3.ejercicios1y2.Persona;
  */
 public class Principal {
     public static void main(String[] args) {
-        ejercicio2();
+//        ejercicio2();
+//        ejercicio3a();
+        ejercicio3b();
+        
     }
     
     public static void ejercicio1(){
@@ -34,5 +38,122 @@ public class Principal {
         Persona p1 = new Persona(nombre, ap1, ap2, edad, nif);
         System.out.println(p1);
         System.out.println("DNI completo: "+p1.getDNI());
+    }
+    
+    
+    public static void ejercicio3a(){
+       
+//        Crea los siguientes objetos:
+//        Coche1: con el estado por defecto.
+//        Coche2: con el siguiente estado:
+//        ◦ encendido = false
+//        ◦ combustible = 80l
+//        ◦ velocidadActual = 0 km/h
+//        ◦ consumo = 7l/100km
+//        ◦ velocidadMax=300 km/h
+//        ◦ combustibleMax=80l
+          Coche coche1 = new Coche();
+          Coche coche2 = new Coche(false, 80, 0, 300, 80, 7);
+//        2. Muestra el estado inicial de ambos coches.
+          System.out.println(coche1);
+          System.out.println(coche2);
+          
+//        3. Enciende ambos coches.
+          coche1.arrancar();
+          coche2.arrancar();
+//        4. Utilizando un bucle avanza el coche1 y acelera hasta que alcances la mitad de su
+//        velocidad máxima. Muestra por pantalla cuantos km han sido necesarios para alcanzar
+//        dicha velocidad.
+          double kmTotales1 = 0;
+          while(coche1.getVelocidadActual()<=coche1.getVelocidadMax()/2){
+              kmTotales1 += coche1.avanzar();
+              coche1.acelerar();
+          }
+          System.out.println("Coche1 avanza "+kmTotales1+"km");
+//        5. Haz lo mismo que en el punto 4 con el coche2.
+          double kmTotales2 = 0;
+          while(coche2.getVelocidadActual()<=coche2.getVelocidadMax()/2){
+              kmTotales2 += coche2.avanzar();
+              coche2.acelerar();
+          }
+          
+          System.out.println("Coche2 avanza "+kmTotales2+"km");
+//        6. Muestra el estado de ambos coches.
+          System.out.println("***********************");
+          System.out.println(coche1);
+          System.out.println(coche2);
+        
+    }
+    
+    public static void ejercicio3b(){
+        // 1. Crea los siguientes objetos:
+        //Coche1: con el siguiente estado:
+        //◦ encendido = false
+        //◦ combustible = 0l
+        //◦ velocidadActual = 0 km/h
+        //◦ consumo = 3.5l/100km
+        //◦ velocidadMax=300 km/h
+        //◦ combustibleMax=80l
+        Coche c1 = new Coche(false, 0, 0, 300, 80, 4.5);
+        //Coche2: con el siguiente estado:
+        //◦ encendido = false
+        //◦ combustible = 0l
+        //◦ velocidadActual = 0 km/h
+        //◦ consumo = 7l/100km
+        //◦ velocidadMax=300 km/h
+        //◦ combustibleMax=110l
+         Coche c2 = new Coche(false, 0, 0, 150, 110, 7);
+        //2. Muestra el estado inicial de ambos coches.
+        System.out.println("------- Coche 1 -------");
+        System.out.println(c1);
+        System.out.println("------- Coche 2 -------");
+        System.out.println(c2);
+        //3. Enciende ambos coches.
+        c1.arrancar();
+        c2.arrancar();
+        //4. Utilizando un bucle avanza con el coche1 hasta que haya recorrido 1000km a la
+        //máxima velocidad posible. Si el coche se queda sin combustible, debe rellenar todo el
+        //depósito, arrancar y volver a acelerar todo lo que pueda.
+        double km = 0;
+        int repostar1 = 0, minutos1 = 0;
+        while(km < 2000){
+            c1.acelerar();
+            km += c1.avanzar();
+            //Cada vez que avanzo lo hago durante 1 minuto
+            minutos1++;
+            if(!c1.isEncendido()){
+                //Repostar
+                c1.setCombustible(c1.getCombustibleMax());
+                repostar1++;
+                c1.arrancar();
+            }
+        }
+        //5. Haz lo mismo con el coche2.
+        km = 0;
+        int repostar2 = 0, minutos2 = 0;
+        while(km < 2000){
+            c2.acelerar();
+            km += c2.avanzar();
+            //Cada vez que avanzo lo hago durante 1 minuto
+            minutos2++;
+            if(!c2.isEncendido()){
+                //Repostar
+                c2.setCombustible(c2.getCombustibleMax());
+                repostar2++;
+                c2.arrancar();
+            }
+        }
+        //6. Apaga ambos vehículos.
+        c1.detener();
+        c2.detener();
+        //7. Al finalizar, por cada coche se mostrará por pantalla cuantas veces ha repostado y los
+        //minutos que ha tardado en recorrer 1000km. También es deseable conocer el estado final
+        //de cada vehículo.
+        
+        System.out.println("FINAL");
+        System.out.println("El coche 1 a repostado "+repostar1+" veces y ha tardado "+minutos1+" minutos.");
+        System.out.println(c1);
+        System.out.println("El coche 2 a repostado "+repostar2+" veces y ha tardado "+minutos2+" minutos.");
+        System.out.println(c2);
     }
 }
---
+++ src/poo3/ejercicio3/Coche.java
@@ -0,0 +1,163 @@
+
+package poo3.ejercicio3;
+
+/**
+ *
+ * @author alumno
+ */
+public class Coche {
+    private boolean encendido;
+    private double combustible;
+    private double velocidadActual;
+    private double velocidadMax;
+    private double combustibleMax;
+    private double consumo;
+
+    public Coche(boolean encendido, double combustible, double velocidadActual, double velocidadMax, double combustibleMax, double consumo) {
+        this.encendido = encendido;
+        this.combustible = combustible;
+        this.velocidadActual = velocidadActual;
+        this.velocidadMax = velocidadMax;
+        this.combustibleMax = combustibleMax;
+        this.consumo = consumo;
+    }
+
+    public Coche() {        
+        this.encendido = false;
+        this.velocidadActual = 0;
+        this.velocidadMax = 230;
+        this.combustibleMax = 100;
+        this.combustible = combustibleMax/2;
+        this.consumo = 5;
+    }
+    
+    public boolean arrancar(){
+        boolean retorno = false;
+        if(combustible > 0){
+            encendido = true;
+            retorno = true;
+        }
+        
+        return retorno;
+    }
+    
+    public boolean acelerar(){
+        boolean retorno = false;
+        double sumaV = velocidadActual+10;
+        if(encendido && sumaV<velocidadMax){
+            velocidadActual = sumaV;
+            retorno = true;
+        }
+        
+        return retorno;
+    }
+    
+    public boolean decelearar(){
+        boolean retorno = false;
+        double restaV = velocidadActual-5;
+        if(encendido && restaV>=0){
+            velocidadActual = restaV;
+            retorno = true;
+        }
+        
+        return retorno;
+    }
+    
+    public double avanzar(){        
+        double km = 0;
+        km = velocidadActual/60; 
+        // l/100km
+        if(combustible >= ((km * consumo)/100)){
+            combustible -= (km * consumo)/100;
+            
+        }else{
+            detener();
+            km = 0; 
+        }
+        
+        return km;        
+    }
+    
+    public void detener(){
+        encendido = false;
+        velocidadActual = 0;
+    }
+
+    public boolean isEncendido() {
+        return encendido;
+    }
+
+    public void setEncendido(boolean encendido) {
+        this.encendido = encendido;
+    }
+
+    public double getCombustible() {
+        return combustible;
+    }
+
+    public void setCombustible(double combustible) {
+        this.combustible = combustible;
+    }
+
+    public double getVelocidadActual() {
+        return velocidadActual;
+    }
+
+    public void setVelocidadActual(double velocidadActual) {
+        this.velocidadActual = velocidadActual;
+    }
+
+    public double getVelocidadMax() {
+        return velocidadMax;
+    }
+
+    public void setVelocidadMax(double velocidadMax) {
+        this.velocidadMax = velocidadMax;
+    }
+
+    public double getCombustibleMax() {
+        return combustibleMax;
+    }
+
+    public void setCombustibleMax(double combustibleMax) {
+        this.combustibleMax = combustibleMax;
+    }
+
+    public double getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(double consumo) {
+        this.consumo = consumo;
+    }
+    
+    public static boolean esCorrectaMatricula(String matricula){
+        boolean correcta = true;
+        if(matricula.length()!=7){
+            correcta = false;
+        }
+        
+        try{
+            Integer.parseInt(matricula.substring(0, 5));
+        }catch(NumberFormatException e){
+            correcta = false;
+        }
+        
+        //TODO letras
+        
+        return correcta;
+    }
+
+    
+    public String toString() {
+        return "Coche{" + "encendido=" + encendido + ", combustible=" + combustible + ", velocidadActual=" + velocidadActual + ", velocidadMax=" + velocidadMax + ", combustibleMax=" + combustibleMax + ", consumo=" + consumo + '}';
+    }
+
+   
+    
+    
+    
+    
+    
+    
+}
